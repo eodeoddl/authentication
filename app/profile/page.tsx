783945742f7df3f61b0ab69c86b1478d90bfcf11
lki <>
2024-03-29 13:16:47 +0900

    auth

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,18 @@
 import { getSession } from "../lib/actions";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-  const session = getSession();
-
+  const session = await getSession();
+  if (!session.isLoggedIn) redirect("/");
   return (
     <div>
       <h1>welcome to profile page</h1>
+      <p>
+        Welcome <b>{session.userName}</b>
+      </p>
+      <span>
+        Yoe are a <b>{session.isPro ? "Premium" : "Free"}</b>
+      </span>
     </div>
   );
 }
